6e988b58642db60b239c8cb5af7751b725651546
GabrielRioo <Gabrielpaivario@gmail.com>
2020-11-02 19:58:45 -0300

    add the class 13 - if, else if, else

---
+++ Aula13/aula13.cs
@@ -0,0 +1,37 @@
+using System;
+
+class Aula12 {
+    static void Main() {
+        int n1,n2,n3,n4, res;
+        n1=n2=n3=n4=res=0;
+
+        Console.Write("Digite a nota 1: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 2: ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 3: ");
+        n3 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite a nota 4: ");
+        n4 = int.Parse(Console.ReadLine());
+
+        res= n1 + n2+ n3+ n4;
+        
+        string resultado = "Reprovado";
+
+
+
+        if(res <= 40) {
+            Console.WriteLine("Reprovado");
+        }
+        else if(res <= 60){
+            Console.WriteLine("Recuperação");
+        }
+        else {
+            Console.WriteLine("Reprovado");
+        }
+
+    }
+}
