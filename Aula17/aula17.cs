265221450665476a8e91f942ab1bf0ed0c00785e
GabrielRioo <Gabrielpaivario@gmail.com>
2020-11-14 18:00:32 -0300

    Add the challage 17 - Arrays

---
+++ Aula17/aula17.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Aula17
+{
+    static void Main()
+    {
+        int[] n = new int[5];
+        // int[] num = new int[3] { 6, 7, 8 };
+        int[] num = {9, 10, 11, 12, 13};
+
+        n[0] = 1;
+        n[1] = 2;
+        n[2] = 3;
+        n[3] = 4;
+        n[4] = 5;
+
+        Console.WriteLine(num[2]);
+    }
+}
